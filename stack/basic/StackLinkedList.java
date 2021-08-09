02ded64498ee9270ae11f40290b3e704f08d2e2f
jhavidit <jhavidit@gmail.com>
2021-08-09 23:19:29 +0530

    stack as linked list

---
+++ stack/basic/StackLinkedList.java
@@ -0,0 +1,70 @@
+package stack.basic;
+
+public class StackLinkedList {
+
+    StackNode top;
+
+    static class StackNode {
+        int data;
+        StackNode next;
+
+        StackNode(int data) {
+            this.data = data;
+            next = null;
+        }
+
+
+    }
+
+    void isEmpty(){
+        System.out.println(top==null);
+    }
+
+    void push(int data){
+        StackNode newNode = new StackNode(data);
+        if(top==null){
+            top = newNode;
+        }
+        else
+        {
+            newNode.next = top;
+            top = newNode;
+        }
+    }
+
+    void pop(){
+        if(top==null)
+            System.out.println("stack underflow");
+       else {
+            System.out.println("Popped = " + top.data);
+            top = top.next;
+        }
+    }
+
+    void display(){
+        if(top==null)
+            System.out.println("stack underflow");
+        else {
+            StackNode temp = top;
+            while(temp!=null){
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        StackLinkedList stack = new StackLinkedList();
+        stack.top = null;
+        stack.push(5);
+        stack.push(6);
+        stack.push(8);
+        stack.pop();
+        stack.display();
+        stack.push(9);
+        stack.display();
+        stack.isEmpty();
+    }
+
+}
