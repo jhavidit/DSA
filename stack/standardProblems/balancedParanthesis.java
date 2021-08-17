d874bffe2d70674ebf63a5de2d350e8d9e510e1b
jhavidit <jhavidit@gmail.com>
2021-08-17 12:41:40 +0530

    balanced paranthesis

---
+++ stack/standardProblems/balancedParanthesis.java
@@ -0,0 +1,31 @@
+package stack.standardProblems;
+
+import java.util.Stack;
+
+public class balancedParanthesis {
+
+    public static boolean checkBalancedParanthesis(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else if (ch == ')') {
+                if (stack.isEmpty() || stack.pop() != '(')
+                    return false;
+            } else if (ch == '}') {
+                if (stack.isEmpty() || stack.pop() != '{')
+                    return false;
+            } else if (ch == ']') {
+                if (stack.isEmpty() || stack.pop() != '[')
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String str = "([{}])";
+        System.out.println(checkBalancedParanthesis(str));
+    }
+}
