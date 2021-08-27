f2c3a6535c6a5ffc273885da99f6cd6d6578ea37
Vidit Jha <jhavidit@gmail.com>
2021-08-27 13:18:57 +0530

    Reverse stack using recursion

--- stack/operations/ReverseStackRecursively.java
+++ stack/operations/ReverseStackRecursively.java
@@ -6,5 +6,37 @@ public class ReverseStackRecursively {
 
     Stack<Integer> stack = new Stack<>();
 
+    void pushAtBottom(int val) {
+        if (stack.isEmpty()) {
+            stack.push(val);
+            return;
+        }
+        int a = stack.peek();
+        stack.pop();
+        pushAtBottom(val);
+        stack.push(a);
+    }
+
+    void reverse() {
+        if (stack.empty())
+            return;
+        int a = stack.peek();
+        stack.pop();
+        reverse();
+        pushAtBottom(a);
+    }
+
+    public static void main(String[] args) {
+        ReverseStackRecursively stack = new ReverseStackRecursively();
+        stack.stack.push(6);
+        stack.stack.push(5);
+        stack.stack.push(12);
+        stack.stack.search(23);
+        stack.stack.push(324);
+        System.out.println(stack.stack);
+        stack.reverse();
+        System.out.println(stack.stack);
+    }
+
 
 }
