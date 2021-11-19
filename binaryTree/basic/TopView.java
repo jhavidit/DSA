e23f844c77459e27fe12689b37113860be1b943e
Vidit Jha <jhavidit@gmail.com>
2021-11-19 14:02:13 +0530

    top view of binary tree

---
+++ binaryTree/basic/TopView.java
@@ -0,0 +1,74 @@
+package binaryTree.basic;
+
+import java.util.*;
+
+public class TopView {
+
+    static class Tuple {
+        Node node;
+        int x;
+        int y;
+
+        Tuple(Node node, int x, int y) {
+            this.x = x;
+            this.y = y;
+            this.node = node;
+        }
+    }
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    Node root;
+
+    TopView(int data) {
+        root = new Node(data);
+    }
+
+    TopView() {
+        root = null;
+    }
+
+
+    public List<Integer> topView(Node root) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        Queue<Tuple> queue = new LinkedList<>();
+        queue.add(new Tuple(root, 0, 0));
+        while (!queue.isEmpty()) {
+            Tuple tuple = queue.poll();
+            Node node = tuple.node;
+            int x = tuple.x;
+            int y = tuple.y;
+            if (!map.containsKey(x)) {
+                map.put(x, node.data);
+            }
+            if (node.left != null) {
+                queue.add(new Tuple(node.left, x - 1, y + 1));
+            }
+            if (node.right != null) {
+                queue.add(new Tuple(node.right, x + 1, y + 1));
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
+
+
+    public static void main(String[] args) {
+        TopView tree = new TopView();
+        tree.root = new Node(3);
+        tree.root.left = new Node(9);
+        tree.root.right = new Node(20);
+        tree.root.right.right = new Node(7);
+        tree.root.right.left = new Node(15);
+        System.out.println(tree.topView(tree.root));
+
+    }
+
+}
